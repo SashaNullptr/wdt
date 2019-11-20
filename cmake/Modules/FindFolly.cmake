68c2ff4c628e1bab4e2e3a9fa4042344a7f5f1dd
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-19 17:16:09 -0700

    Add find-folly module.

---
+++ cmake/Modules/FindFolly.cmake
@@ -0,0 +1,83 @@
+#.rst:
+#
+# Adapted from https://github.com/omnisci/omniscidb/blob/master/cmake/Modules/FindFolly.cmake
+# Carries the Apache 2.0 License
+#
+# FindFolly.cmake
+# -------------
+#
+# Find a Folly installation.
+#
+# This module finds if Folly is installed and selects a default
+# configuration to use.
+#
+# find_package(Folly ...)
+#
+#
+# The following variables control which libraries are found::
+#
+#   FOLLY_USE_STATIC_LIBS  - Set to ON to force use of static libraries.
+#
+# The following are set after the configuration is done:
+#
+# ::
+#
+#   FOLLY_FOUND            - Set to TRUE if Folly was found.
+#   FOLLY_LIBRARIES        - Path to the Folly libraries.
+#   FOLLY_LIBRARY_DIRS     - compile time link directories
+#   FOLLY_INCLUDE_DIRS     - compile time include directories
+#
+#
+# Sample usage:
+#
+# ::
+#
+#    find_package(Folly)
+#    if(FOLLY_FOUND)
+#      target_link_libraries(<YourTarget> ${FOLLY_LIBRARIES})
+#    endif()
+
+if(FOLLY_USE_STATIC_LIBS)
+    set(_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES .lib .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
+endif()
+
+
+find_library(FOLLY_LIBRARY
+        NAMES folly
+        HINTS
+        ENV LD_LIBRARY_PATH
+        ENV DYLD_LIBRARY_PATH
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        /usr/local/homebrew/lib
+        /opt/local/lib)
+
+get_filename_component(FOLLY_LIBRARY_DIR ${FOLLY_LIBRARY} DIRECTORY)
+
+find_library(FOLLY_DC_LIBRARY
+        NAMES double-conversion
+        HINTS
+        ENV LD_LIBRARY_PATH
+        ENV DYLD_LIBRARY_PATH
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        /usr/local/homebrew/lib
+        /opt/local/lib)
+
+find_package(OpenSSL REQUIRED)
+
+if(FOLLY_USE_STATIC_LIBS)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${_CMAKE_FIND_LIBRARY_SUFFIXES})
+endif()
+
+# Set standard CMake FindPackage variables if found.
+set(FOLLY_LIBRARIES ${FOLLY_LIBRARY} ${FOLLY_DC_LIBRARY} ${OPENSSL_LIBRARIES} ${CMAKE_DL_LIBS})
+set(FOLLY_LIBRARY_DIRS ${FOLLY_LIBRARY_DIR})
+get_filename_component(absIncludeDir "../include" REALPATH BASE_DIR "${FOLLY_LIBRARY_DIR}")
+set(FOLLY_INCLUDE_DIRS ${absIncludeDir})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Folly REQUIRED_VARS FOLLY_LIBRARY FOLLY_DC_LIBRARY)
